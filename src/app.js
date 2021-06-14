4ad03249e457924a470d3cc7d36433d7a2000b0b
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-06-14 21:45:16 +0200

    (bug): fixing timing 2

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const client = new Discord.Client({partials: ['MESSAGE']
 
 client.on('ready', () => {
     console.log("Client connected 🚀🚀🚀");
-    client.channels.cache.find(channel => channel.name === "standups").send("connected again");
+    client.channels.cache.find(channel => channel.name === "standups").send("connected: "+new Date());
 
 });
 let scheduledMessage = new cron.CronJob('00 26 16 * * *', () => {
