6a0d6e36cc6a448ba1904491e0903a7740c73bca
PatrickNiyogitare28 <patrickniyogitare28@gmail.com>
2021-06-17 06:18:22 +0200

    (bug): change standup time

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ client.on('ready', () => {
     console.log("Client connected 🚀🚀🚀");
 });
 
-let scheduledMessage = new cron.CronJob('00 12 04 * * *', () => {
+let scheduledMessage = new cron.CronJob('00 00 05 * * *', () => {
     let channel = client.channels.cache.find(channel => channel.name === "standups");
     if(!channel)
      return;
