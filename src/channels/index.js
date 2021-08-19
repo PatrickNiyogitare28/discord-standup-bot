9a6a0c2d2c8ab415344f4185c59ae50adeeba64f
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2021-08-19 08:52:18 +0200

    chore() modified channels

--- src/channels/index.js
+++ src/channels/index.js
@@ -3,10 +3,10 @@ export const channels = [
         name:'standups',
         id:'779669129057665044'
     },
-    // {
-    //     name:'technophiles',
-    //     id:'872037848633724938'
-    // },
+    {
+        name:'technophiles',
+        id:'872037848633724938'
+    },
     {
         name:'pana-african-devs',
         id:'872400629551755306'
